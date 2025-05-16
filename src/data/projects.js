033596a0059f9e410eb26e95350d65af059d6e4d
Iyobosa <omosiyobo@gmail.com>
2025-05-16 20:48:02 +0100

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,6 +2,9 @@ import todo from "../assets/simple-mern-todo.png";
 import responsive from "../assets/responsive-social-media-dashboard.png";
 import summit from "../assets/sca-summit-2022.png";
 import weventa from "../assets/weventa.png";
+import jamzy from "../assets/jamzy_studio.png";
+import sure from "../assets/sure-boi-photography.png";
+import email from "../assets/email-server.png";
 
 export const projects = [
   {
@@ -19,10 +22,31 @@ export const projects = [
     code: "https://github.com/Fhave/weventa",
   },
   {
+    img: jamzy,
+    title: "Jamzy Studio Website",
+    desc: "Jamzy Studio Website. A simple portfolio website for a photographer with contact form. I also added some SEO optimization and made sure to verify the website on Google. The necessary meta tags were added.",
+    live: "https://jamzy-studio.netlify.app/",
+    code: "https://github.com/Fhave/jamzy-studio",
+  },
+  {
+    img: sure,
+    title: "Sure Boi Photograhy Website",
+    desc: "Sure Boi Photograhy Website.  A simple portfolio website for a photographer with contact form. Animatio was added to the website.",
+    live: "https://sure-boi-photography.netlify.app/",
+    code: "https://github.com/Fhave/sure-boi-photography",
+  },
+  {
     img: todo,
     title: "Simple ToDo App",
     desc: "Simple ToDo App. Built using MERN Stack",
     live: "https://dazzling-biscuit-24a626.netlify.app/#!",
     code: "https://github.com/Fhave/todo_server",
+  },
+  {
+    img: email,
+    title: "Simple Email Server",
+    desc: "Simple Email Server. This was used in other projects for the contact form.",
+    live: "https://email-server-ochre.vercel.app/",
+    code: "https://github.com/Fhave/email_server",
   },
 ];
